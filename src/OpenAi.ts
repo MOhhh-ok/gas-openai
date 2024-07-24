3c269614c8c768230eb36834709b0846ed0d345e
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2024-07-25 08:15:47 +0900

    -

---
+++ src/OpenAi.ts
@@ -0,0 +1,30 @@
+type OpenAiModel = 'gpt-3.5-turbo' | 'gpt-4';
+type OpenAiMessage = { role: 'user' | 'assistant'; content: string };
+type OpenAiResponse = { choices: [{ message: { content: string } }] };
+
+class OpenAi {
+    constructor(private apiKey: string) {}
+
+    completions(params: {
+        messages: OpenAiMessage[];
+        model: OpenAiModel;
+    }): OpenAiResponse {
+        const { messages, model } = params;
+        const url = 'https://api.openai.com/v1/chat/completions';
+        const headers = {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + this.apiKey,
+        };
+        const body = {
+            model,
+            messages,
+        };
+        const response = UrlFetchApp.fetch(url, {
+            method: 'post',
+            headers: headers,
+            payload: JSON.stringify(body),
+        });
+        const result = JSON.parse(response.getContentText());
+        return result;
+    }
+}
